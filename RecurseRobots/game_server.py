9b3468a9623a836ba3871ec281fdb26544837d7f
Karan Tamhane <karan.tamhane@gmail.com>
2016-03-03 14:13:39 -0500

    initial game server skeleton

---
+++ RecurseRobots/game_server.py
@@ -0,0 +1,19 @@
+
+class Server():
+	def __init__(self):
+		pass
+
+	def register_robot(self, robot_description):
+		pass
+
+	def get_position(self, robot):
+		pass
+
+	def request_move(self, magnitude, direction):
+		pass
+
+	def attack(self, robot):
+		pass
+
+	def observe(self, robot):
+		pass
