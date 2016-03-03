089c64c8064b1fe5f24b5fe05579a618460816e6
Karan Tamhane <karan.tamhane@gmail.com>
2016-03-03 14:15:01 -0500

    initial game server skeleton

--- RecurseRobots/game_server.py
+++ RecurseRobots/game_server.py
@@ -4,16 +4,21 @@ class Server():
 		pass
 
 	def register_robot(self, robot_description):
+		# Instantiate and return robot object as per description
 		pass
 
 	def get_position(self, robot):
+		# Return robot position
 		pass
 
 	def request_move(self, magnitude, direction):
+		# Check if requested move is legal and return updated position
 		pass
 
 	def attack(self, robot):
+		# Perform attack move for robot and return results
 		pass
 
 	def observe(self, robot):
+		# Return observations for robot based on sensor capabilities
 		pass
