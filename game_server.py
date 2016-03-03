8efc0224767ebed1b27263f5dc7466161670fc61
Joe Ardent <code@ardent.nebcorp.com>
2016-03-03 15:53:56 -0500

    new shit has come to light

--- game_server.py
+++ game_server.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 
+import recurse_robot_broker as broker
 import recurse_robot_client as RRC
 
 class Server(Object):
   def __init__(self, config):
     self.config = config # config is some object?
     self.robots = {}
+    self.broker = broker.Broker(self)
 
+  # These methods are remote-facing
   def register_robot(self, robot_description, conn):
     # Instantiate and return robot object as per description
     r = RRC.Robot(robot_description)
     robot_id = RRC.make_id(r)
     self.robots[robot_id] = r
     msg = make_message(query=register, tipe=reply, body=robot_id)
-    send_message(r, msg)
+    self.broker.send_message(r, msg)
 
   def get_position(self, robot_id):
     # Return robot position
     r = self.robots[robot_id]
     msg = make_message(query=position, tipe=reply, body=r.position)
-    send_message(r, msg)
+    self.broker.send_message(r, msg)
 
   def request_move(self, robot_id, magnitude, direction):
     # Check if requested move is legal and return updated position
